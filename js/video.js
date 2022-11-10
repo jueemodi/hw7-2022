f19ece266cc4c0396a29bab75c4cdff145c92a08
jueemodi <98283520+jueemodi@users.noreply.github.com>
2022-11-10 00:25:38 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,87 @@
+var video;
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+	console.log("autoplay is set to " + video.autoplay);
+	console.log("loop is set to " + video.loop);
+	
+	//missed something lecture 
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	video.play(); 
+	document.querySelector("volume").innerHTML = video.volume * 100 + '%'; 
+ });
+
+ document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+    video.pause(); 
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("slow video")
+	video.playbackRate = 0.9;
+	console.log("new speed = " + video.playbackRate);
+
+});
+
+//why won't this word 
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("slow video")
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log("slower speed = " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("speed up video")
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log("faster speed = " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("skip 10 s ahead");
+	if (video.currentTime > 70) {
+		video.currentTime = 0;
+	} else {
+		video.currentTime += 10; 
+	}
+	console.log("current time = " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("mute");
+	if (video.muted === false) {
+		video.muted = true;
+		document.querySelector('#mute').innerHTML = 'Unmute';
+	} else {
+		video.muted = false;
+	}
+});
+
+
+//volume slider
+document.querySelector("#slider").addEventListener("change", function() {
+	var playVol = document.getElementById("slider").value; //i think this is wrong
+	console.log("before vol: " + video.volume);
+	video.volume = playVol / 100; 
+	console.log("after vol: " + video.volume);
+
+	//update volume is
+	document.querySelector('#volume').innerHTML = playVol + '%'; // not wprking 
+
+});
+
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	document.getElementById('player1').className += " oldSchool";
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	document.getElementById('player1').classList.remove('oldSchool');
+});
+
+
